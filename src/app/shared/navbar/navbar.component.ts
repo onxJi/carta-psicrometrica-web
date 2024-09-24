51c4ff399258b222b662bab95abdefc1efb79ae4
JAAZIEL JIMENEZ MIGUEL <jaaziel-jz77@outlook.com>
2024-09-24 13:28:51 -0700

    navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, ElementRef, viewChild } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   host: {
-    class: "bg-white border-gray-200 dark:bg-gray-900"
+    class: "border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
   }
 })
 export class NavbarComponent {
@@ -15,6 +15,6 @@ export class NavbarComponent {
   public contentValue = viewChild<ElementRef<HTMLDivElement>>('navbarCta');
 
   abrirMenu() {
-    this.contentValue()?.nativeElement
+    this.contentValue()?.nativeElement.classList.toggle('hidden');
   }
 }
