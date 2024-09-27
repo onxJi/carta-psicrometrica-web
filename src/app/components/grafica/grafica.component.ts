1b11aa46c100352c9e927b68fdddc2e93236eb85
JAAZIEL JIMENEZ MIGUEL <jaaziel-jz77@outlook.com>
2024-09-27 16:29:15 -0700

    calculo de las lineas de Tbh

--- src/app/components/grafica/grafica.component.ts
+++ src/app/components/grafica/grafica.component.ts
@@ -20,7 +20,7 @@ export class GraficaComponent implements OnInit {
   data: any;
   ecuations = new Ecuations();
   options: any;
-  datasets = signal<any[]>([]);
+  datasets: any[] = [];
   altitudValue = signal(0);
   tbs_range = Array.from({ length: 15 }, (_, i) => i * 5 - 10);
   hr_range = Array.from({ length: 11 }, (_, i) => i * 10);
@@ -34,9 +34,11 @@ export class GraficaComponent implements OnInit {
       const altitudValue = altitud ?? 0;
       this.altitudValue.set(altitudValue);
       // Limpiar el datasets antes de calcular la gráfica
-      this.datasets.set([]); // Limpiar datasets
+      this.datasets = []; // Limpiar datasets
 
-      this.calcularGrafica(); // Llamar a la función para calcular los nuevos datos
+      this.calcularGrafica(); 
+      this.calculoLineasTbh();
+      this.calculoLineasHumedad();
     });
 
     const documentStyle = getComputedStyle(document.documentElement);
@@ -45,12 +47,18 @@ export class GraficaComponent implements OnInit {
     const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
     this.data = {
       labels: this.tbs_range,
-      datasets: this.datasets()
+      datasets: this.datasets
     };
 
     this.options = {
+      type: 'line',
       maintainAspectRatio: false,
       aspectRatio: 0.6,
+      elements: {
+        point: {
+          radius: 0.5
+        }
+      },
       plugins: {
         legend: {
           labels: {
@@ -69,6 +77,8 @@ export class GraficaComponent implements OnInit {
           ticks: {
             stepSize: 1
           },
+          type: 'linear',  // Asegúrate de que el eje X sea lineal
+          position: 'bottom',
           min: 0,
           max: 60
         },
@@ -79,10 +89,12 @@ export class GraficaComponent implements OnInit {
             text: 'Entalpía (kJ/kg)'
           },
           min: 0,
-          max: 100
+          max: 100,
+          type: 'linear',
         },
         y1: {
           beginAtZero: true,
+          type: 'linear',
           position: 'right',
           title: {
             display: true,
@@ -95,6 +107,7 @@ export class GraficaComponent implements OnInit {
           display: false,
           beginAtZero: true,
           position: 'right',
+          type: 'linear',
           min: 0,
           max: 0.04,
         },
@@ -108,18 +121,9 @@ export class GraficaComponent implements OnInit {
     let data: any[] = [];
     const documentStyle = getComputedStyle(document.documentElement);
 
-
-    this.hr_range.forEach(hr => {
-      this.tbs_range.forEach(tbs => {
-        const result_ = this.ecuations.main(tbs, hr, alt);
-
-      });
-    });
-
     this.tbs_range.forEach(tbs => {
       const { pv, W, U, Tbh, Tr, Veh, h, pvs, Ws } = this.ecuations.main(tbs, 100, alt);
-      console.log({ pv, W, U, Tbh, Tr, Veh, h, pvs, Ws });
-      this.datasets().push({
+      this.datasets.push({
         label: `Tbs = ${tbs}`,  // Etiqueta para identificar cada serie
         data: [
           {
@@ -140,5 +144,46 @@ export class GraficaComponent implements OnInit {
       });
     });
 
+  }
+
+
+  calculoLineasHumedad() {
+    const results_by_hr: { [key: number]: any[] } = {};
+    let W_range: any = {};
+    const Tbh_range: { [key: number]: number[] } = {};
+
+    this.hr_range.forEach(hr => {
+
+      this.tbs_range.forEach(tbs => {
+        const { W} = this.ecuations.main(tbs, hr, this.altitudValue());
+        
+        W_range[hr].push(W);
+        console.log(W_range)
+      });
+    });
+
+    
+  }
+
+  calculoLineasTbh() {
+    const tbhRange: { tbs: number, Yas: number, tg: number, Ya: number }[] = [];
+
+    this.tbs_range.forEach((tbs) => {
+      const result_ = this.ecuations.main(tbs, 100, this.altitudValue());
+      const lambdaV = this.ecuations.lamba_v(tbs); 
+      const Yas = result_.Ws;
+      //tbhRange.push({ tbs, Yas, tg: (((lambdaV / 0.227) * Yas) + tbs), Ya: 0 });
+      const tg = (((lambdaV / 0.227) * Yas) + tbs);
+      this.datasets.push({
+          label: `Tbh = ${result_.Tbh}`,  // Etiqueta para identificar cada serie
+          data: [{ x: tg, y: 0 }, { x: tbs, y: Yas }],
+          showLine: true,  // Muestra líneas en lugar de puntos sueltos
+          backgroundColor: 'rgb(255, 100, 0)',  // Color de los puntos o línea
+          borderColor: 'rgb(255, 100, 0)',      // Color de la línea
+          borderWidth: 1,    // Grosor de la línea
+          yAxisID: 'y1'      // Definimos el eje Y que queremos usar
+      })
+    });
+    console.log(this.datasets)
   }
 }
